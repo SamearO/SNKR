4cd15b51a16151edb87d600f4710a2904b0fe7da
Samir Ouarbya <ouarbyasamir@gmail.com>
2020-09-22 10:48:41 +0100

    Data Class Added

--- src/components/MyAreaChart.js
+++ src/components/MyAreaChart.js
@@ -47,62 +47,67 @@ export const MyAreaChart = (props) => {
   // sets the data to be displayed as data from the API
   var data = db;
 
-  // This just formats  all the dates from the file from ISO format to YYYY-MM-DD format which makes it easier to read for the user
-  function formatdate() {
-    moment().format();
-    for (var i = 0; i < data.length; i++) {
-      data[i].ProductActivity__createdAt = moment(
+  class Data{
+    // This just formats  all the dates from the file from ISO format to YYYY-MM-DD format which makes it easier to read for the user
+    static formatdate(){
+      moment().format();
+      for (var i = 0; i < data.length; i++) {
+        data[i].ProductActivity__createdAt = moment(
         data[i].ProductActivity__createdAt
-      ).format("YYYY-MM-DD");
+        ).format("YYYY-MM-DD");
+    }
+    }
+    // this code sorts data so that it is in the correct order of dates
+    static sortDates(){
+      data.sort(function compare(a, b) {
+        var dateA = new Date(a.ProductActivity__createdAt);
+        var dateB = new Date(b.ProductActivity__createdAt);
+        return dateA - dateB;
+      });
+    }
+    // this function returns an array of sales between the dates passed as a paramater
+    static filterDates(startDate, endDate){
+      var filtered2 = [];
+      var datetofilter1 = new Date(startDate);
+      datetofilter1 = datetofilter1.getTime();
+      var datetofilter2 = new Date(endDate)
+      datetofilter2 = datetofilter2.getTime();
+      for (var i = 0; i < data.length; i++) {
+        var comparisondate = new Date(data[i].ProductActivity__createdAt);
+        if (comparisondate.getTime() < datetofilter2 && comparisondate.getTime() > datetofilter1) {
+          filtered2.push(data[i]);
+        }
+      }
+      return filtered2;
+    }
+    // this function returns an array of sales after filtering by the two paramaters
+    static filterByDateAndSize(size, startDate, endDate){
+      var filtered = [];
+      var datetofilter1 = new Date(startDate);
+      datetofilter1 = datetofilter1.getTime();
+      var datetofilter2 = new Date(endDate);
+      datetofilter2 = datetofilter2.getTime();
+      for (var i = 0; i < data.length; i++) {
+        var comparisondate = new Date(data[i].ProductActivity__createdAt);
+        if (
+          data[i]["ProductActivity__shoeSize"] == size &&
+          comparisondate.getTime() < datetofilter2 && comparisondate.getTime() > datetofilter1
+        ) {
+          filtered.push(data[i]);
+        } else if (size == 0 && comparisondate.getTime() < datetofilter2 && comparisondate.getTime() > datetofilter1) {
+          filtered = Data.filterDates(startDate, endDate);
+        }
+      }
+      return filtered;
     }
   }
 
-  // this code sorts data so that it is in the correct order of dates
-  data.sort(function compare(a, b) {
-    var dateA = new Date(a.ProductActivity__createdAt);
-    var dateB = new Date(b.ProductActivity__createdAt);
-    return dateA - dateB;
-  });
-
-  // this function returns an array of sales up to the date that is passed as a paramater
-  function filterdate(startDate, endDate) {
-    var filtered2 = [];
-    var datetofilter1 = new Date(startDate);
-    datetofilter1 = datetofilter1.getTime();
-    var datetofilter2 = new Date(endDate)
-    datetofilter2 = datetofilter2.getTime();
-    for (var i = 0; i < data.length; i++) {
-      var comparisondate = new Date(data[i].ProductActivity__createdAt);
-      if (comparisondate.getTime() < datetofilter2 && comparisondate.getTime() > datetofilter1) {
-        filtered2.push(data[i]);
-      }
-    }
-    return filtered2;
-  }
-  // this function returns an array of sales after filtering by the two paramaters
-  function filterbydateandsize(size, startDate, endDate) {
-    var filtered = [];
-    var datetofilter1 = new Date(startDate);
-    datetofilter1 = datetofilter1.getTime();
-    var datetofilter2 = new Date(endDate);
-    datetofilter2 = datetofilter2.getTime();
-    for (var i = 0; i < data.length; i++) {
-      var comparisondate = new Date(data[i].ProductActivity__createdAt);
-      if (
-        data[i]["ProductActivity__shoeSize"] == size &&
-        comparisondate.getTime() < datetofilter2 && comparisondate.getTime() > datetofilter1
-      ) {
-        filtered.push(data[i]);
-      } else if (size == 0 && comparisondate.getTime() < datetofilter2 && comparisondate.getTime() > datetofilter1) {
-        filtered = filterdate(startDate, endDate);
-      }
-    }
-    return filtered;
-  }
+  Data.sortDates()
+  
   // sets data to data filtered by the filtered component
-  data = filterbydateandsize(props.size, props.startDate, props.endDate);
+  data = Data.filterByDateAndSize(props.size, props.startDate, props.endDate);
 
-  formatdate();
+  Data.formatdate();
 
   console.log("Data being displayed:", data);
 
